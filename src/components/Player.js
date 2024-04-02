f70121b0930868377ea782d7a84ebcf67da53f99
Abdulrhman Aliyan <aboood1999@live.com>
2024-04-02 21:30:04 +0300

    added two-way-binding to an player name input so the player can change the name and the default name is the initalName that was passed as prop to the player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,34 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        console.log("Edit button clicked");
+        setIsEditing(editing => !editing);
+    }
+
+    function handleChange(event) {
+        console.log('event: ' + event)
+        console.log('event target: ' + event.target)
+        console.log('event target value: ' + event.target.value)
+        setPlayerName(event.target.value)
+    }
+
+    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    }
+
     return (
         <li>
             <span className="player">
-                <span className='player-name'>{name}</span>
+                {editablePlayerName}
                 <span className='player-symbol'>{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
-    )
+    );
 }
