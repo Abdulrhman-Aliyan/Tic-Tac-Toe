30aedd78d969c5e1b490cbbb922ebb8ef3cfb12e
Abdulrhman Aliyan <aboood1999@live.com>
2024-04-03 21:57:03 +0300

    multi dimensional array to use it for the tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 
 export default function App() {
@@ -12,7 +13,7 @@ export default function App() {
           <Player initialName="Player 2" symbol="O"/>
         </ol>
 
-        GAME BOARD
+        <GameBoard/>
        </div>
 
        LOG
